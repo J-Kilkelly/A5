1ef33ff993f564b36d8475b81406008e26293846
John Kilkelly <JKilkelly@live.com>
2025-02-04 22:41:01 -0500

    COP2360 A5 - reverse and capitalize a string

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace KilkellyJA5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A5 revString = new A5(); //new instance of the A5 class
+        string output = revString.ReverseCapitalize("abcdef");
+        Console.WriteLine(output);
+
+    }
+}
+    public class A5
+    {
+        public string ReverseCapitalize(string initialString)
+        {
+            char[] charArray = initialString.ToCharArray(); //create an array of the characters in the argument to the initialString parameter
+            Array.Reverse(charArray); //reverse characters in the array
+            string reverseString = new string(charArray).ToUpper(); //capitalize the (reversed) array
+            return reverseString;
+        }
+    }
